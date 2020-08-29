821602b5f0aeccfcd50801dbb2f872fe582d8da5
Goden <goden.incredible@gmail.com>
2020-08-29 15:38:01 +0800

    #update

---
+++ Scripts/utils.js
@@ -0,0 +1,13 @@
+﻿function Hello() {
+    alert("Hello World!!");
+}
+
+function isAnInteger(text) {
+    var intTestRegex = /^\s*(\+|-)?\d+\s*$/;
+    return String(text).search(intTestRegex) != -1;
+}
+
+function isSignificant(text) {
+    var notWhitespaceTestRegex = /[^\s]{1,}/;
+    return String(text).search(notWhitespaceTestRegex) != -1;
+}
